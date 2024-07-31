dac6eea58238ab5b873bb23766e66cd02a701ff5
Eka Winata <eka.winata@gopay.co.id>
2024-07-31 14:08:30 +0700

    Add CEL string, math and binding library

--- src/test/java/com/gotocompany/firehose/proto/ProtoToMetadataMapperTest.java
+++ src/test/java/com/gotocompany/firehose/proto/ProtoToMetadataMapperTest.java
@@ -18,10 +18,13 @@ public class ProtoToMetadataMapperTest {
     public void setup() {
         Map<String, String> template = new HashMap<>();
         template.put("$GenericResponse.detail", "$GenericResponse.success");
+        template.put("detail", "$GenericResponse.detail.lowerAscii()");
         template.put("someField", "someValue");
         template.put("$GenericResponse.success", "staticValue");
         template.put("staticKey", "$(GenericResponse.errors[0].cause + '-' + GenericResponse.errors[0].code + '-' + string(GenericResponse.code))");
         template.put("entity", "$GenericResponse.errors[0].entity");
+        template.put("binding", "$cel.bind(code, GenericResponse.code, code + 100)");
+        template.put("math", "$math.greatest(GenericResponse.code, 200)");
         this.protoToMetadataMapper = new ProtoToMetadataMapper(
                 GenericResponse.getDescriptor(),
                 template
@@ -32,7 +35,7 @@ public class ProtoToMetadataMapperTest {
     public void shouldBuildDynamicMetadataWithCorrectPlaceholders() {
         GenericResponse payload = GenericResponse.newBuilder()
                 .setSuccess(false)
-                .setDetail("detail_of_error")
+                .setDetail("Detail_Of_Error")
                 .setCode(100)
                 .addErrors(GenericError.newBuilder()
                         .setCode("404")
@@ -44,6 +47,8 @@ public class ProtoToMetadataMapperTest {
 
         Assertions.assertTrue(metadata.containsKey(Metadata.Key.of("detail_of_error", Metadata.ASCII_STRING_MARSHALLER)));
         Assertions.assertEquals("false", metadata.get(Metadata.Key.of("detail_of_error", Metadata.ASCII_STRING_MARSHALLER)));
+        Assertions.assertTrue(metadata.containsKey(Metadata.Key.of("detail", Metadata.ASCII_STRING_MARSHALLER)));
+        Assertions.assertEquals("detail_of_error", metadata.get(Metadata.Key.of("detail", Metadata.ASCII_STRING_MARSHALLER)));
         Assertions.assertTrue(metadata.containsKey(Metadata.Key.of("statickey", Metadata.ASCII_STRING_MARSHALLER)));
         Assertions.assertEquals("not_found-404-100", metadata.get(Metadata.Key.of("statickey", Metadata.ASCII_STRING_MARSHALLER)));
         Assertions.assertTrue(metadata.containsKey(Metadata.Key.of("somefield", Metadata.ASCII_STRING_MARSHALLER)));
@@ -52,6 +57,10 @@ public class ProtoToMetadataMapperTest {
         Assertions.assertEquals("", metadata.get(Metadata.Key.of("entity", Metadata.ASCII_STRING_MARSHALLER)));
         Assertions.assertTrue(metadata.containsKey(Metadata.Key.of("false", Metadata.ASCII_STRING_MARSHALLER)));
         Assertions.assertEquals("staticValue", metadata.get(Metadata.Key.of("false", Metadata.ASCII_STRING_MARSHALLER)));
+        Assertions.assertTrue(metadata.containsKey(Metadata.Key.of("binding", Metadata.ASCII_STRING_MARSHALLER)));
+        Assertions.assertEquals("200", metadata.get(Metadata.Key.of("binding", Metadata.ASCII_STRING_MARSHALLER)));
+        Assertions.assertTrue(metadata.containsKey(Metadata.Key.of("math", Metadata.ASCII_STRING_MARSHALLER)));
+        Assertions.assertEquals("200", metadata.get(Metadata.Key.of("math", Metadata.ASCII_STRING_MARSHALLER)));
     }
 
     @Test
